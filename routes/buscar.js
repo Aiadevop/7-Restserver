9ecaea4a10270bb1d7ffa7e90906d6bbc212da68
Lara <almudena85perez@gmail.com>
2022-07-18 08:55:50 +0200

    p15

--- routes/buscar.js
+++ routes/buscar.js
@@ -3,9 +3,7 @@ const { buscar } = require('../controllers/buscar.controller');
 
 const router = Router();
 
-router.get('/:coleccion/:termino', [
-    
-],buscar);
+router.get('/:coleccion/:termino',buscar);
 
 
 module.exports = router;
