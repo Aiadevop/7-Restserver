11ba080daf8ea2219291ccab0872fa7db5916e03
Lara <almudena85perez@gmail.com>
2022-07-27 07:46:33 +0200

    Guardar imagenes en nuestro servidor

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -25,7 +25,9 @@ const cargarArchivo = async (req, res = response) => {
         const nombre = await subirArchivo(req.files, undefined, 'carpeta-imagenes');
         res.json({ nombre })
     } catch (msg) {
-        res.status(400).json({ msg })
+        //res.status(400).json({ msg })
+        const imgNotFound = path.join(__dirname, '../assets/no-image.jpg')
+        return res.sendFile(imgNotFound)
     }
 }
 
@@ -72,6 +74,7 @@ const actualizarImagen = async (req, res = response) => {
                 fs.unlinkSync(pathImagen)
             }
         }
+
         
     } catch (error) {
         res.status(500).json({ msg: 'No se han podido limpiar las imágenes' })
@@ -90,7 +93,60 @@ const actualizarImagen = async (req, res = response) => {
 
 }
 
+const mostrarImagen = async (req, res= response) => {
+    
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                });
+            }
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' })
+      
+    }
+    
+
+    // Limpiar imágenes previas.
+
+    try {
+
+        if (modelo.img) {
+            //Borrar imagen del servidor
+            const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+            if (fs.existsSync(pathImagen)) {
+               return res.sendFile(pathImagen)
+            }
+        }
+        const imgNotFound = path.join(__dirname, '../assets/no-image.jpg')
+        return res.sendFile(imgNotFound)
+        
+    } catch (error) {
+        res.status(500).json({ msg: 'No se han podido limpiar las imágenes' })
+    }    
+    
+
+}
 module.exports = {
     cargarArchivo,
-    actualizarImagen
+    actualizarImagen,
+    mostrarImagen
 };
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,12 +1,18 @@
 const { Router } = require ('express');
 const { check } = require('express-validator');
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads.controller');
+const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads.controller');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarArchivoSubir } = require('../middlewares');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
+
+router.get('/:coleccion/:id',[
+    check('id','El id debe se de Mongo').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas (c, ['usuarios','productos'])),
+    validarCampos
+], mostrarImagen)
 
 router.post ('/',validarArchivoSubir, cargarArchivo);
 
